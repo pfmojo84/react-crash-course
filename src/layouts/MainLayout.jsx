bdd7372b4183f1e1306eddcdf1e6d352bbc4796a
jdporr <jdporrazzo@gmail.com>
2024-05-13 14:28:03 -0400

    create add, delete, update pages, and toastify functions

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import NavBar from '../components/Navbar'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import NavBar from '../components/Navbar';
 
 const MainLayout = () => {
   return (
     <>
       <NavBar />
       <Outlet />
+      <ToastContainer />
     </>
   )
 }
--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,45 +1,48 @@
 import React from 'react';
+import { useParams, useLoaderData, Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
-const AddJobPage = ({ addJobSubmit }) => {
-  const [title, setTitle] = useState('');
-  const [type, setType] = useState('Full-Time');
-  const [location, setLocation] = useState('');
-  const [description, setDescription] = useState('');
-  const [salary, setSalary] = useState('Under $50K');
-  const [companyName, setCompanyName] = useState('');
-  const [companyDescription, setCompanyDescription] = useState('');
-  const [contactEmail, setContactEmail] = useState('');
-  const [contactPhone, setContactPhone] = useState('');
 
-  const navigate = useNavigate();
+const EditJobPage = ({ updateJobSubmit }) => {
+        const job = useLoaderData();
+        const [title, setTitle] = useState(job.title);
+        const [type, setType] = useState(job.type);
+        const [location, setLocation] = useState(job.location);
+        const [description, setDescription] = useState(job.description);
+        const [salary, setSalary] = useState(job.salary);
+        const [companyName, setCompanyName] = useState(job.company.name);
+        const [companyDescription, setCompanyDescription] = useState(job.company.description);
+        const [contactEmail, setContactEmail] = useState(job.company.contactEmail);
+        const [contactPhone, setContactPhone] = useState(job.company.contactPhone);
 
+        const navigate = useNavigate();
+        const { id } = useParams();
+        
+        const submitForm = (e) => {
+            e.preventDefault();
 
-  const submitForm = (e) => { 
-    e.preventDefault();
-
-    const newJob = {
-        title,
-        type,
-        location,
-        description,
-        salary,
-        company: {
-            name: companyName,
-            description: companyDescription,
-            contactEmail,
-            contactPhone
+        const updatedJob = {
+            id,
+            title,
+            type,
+            location,
+            description,
+            salary,
+            company: {
+                name: companyName,
+                description: companyDescription,
+                contactEmail,
+                contactPhone
         },
     };
-    addJobSubmit(newJob);
+    updateJobSubmit(updatedJob);
 
-    toast.success('Job added successfully');
+    toast.success('Job updated successfully');
 
-    return navigate('/jobs');
-  };
-
+    return navigate(`/jobs/${id}`);
+        }
+    
   return (
     <section className='bg-indigo-50'>
     <div className='container m-auto max-w-2xl py-24'>
@@ -233,8 +236,7 @@ const AddJobPage = ({ addJobSubmit }) => {
       </div>
     </div>
   </section>
-
   )
 }
 
-export default AddJobPage
+export default EditJobPage
